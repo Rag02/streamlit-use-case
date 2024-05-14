a304193c1f27c1ee89968a1f3c35afc569feae38
Raghad Alharbi <Raghad.alharbi02@gmail.com>
2024-05-14 13:34:03 +0300

    .

--- app.py
+++ app.py
@@ -2,4 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+df = pd.read_csv('Data\clean.csv')
+
+st.title('Find Your Perfect Used Car – Great Prices and Financing Options!')
+st.markdown("*Streamlit* is **really** ***cool***.")
+
